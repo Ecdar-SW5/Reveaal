3c186410733586a19bfde3121d158a794db8b0ed
Puvikaran2001 <74664443+Puvikaran2001@users.noreply.github.com>
2022-10-04 12:04:07 +0200

    Fix Check code is formatted
    Have formatted the code with "cargo fmt"

--- src/System/reachability.rs
+++ src/System/reachability.rs
@@ -1,39 +1,39 @@
 use crate::ModelObjects::component::{State, Transition};
 use crate::TransitionSystems::TransitionSystem;
 
-
-pub struct SubPath{
-    start_state : State,
-    transition : Transition,
+pub struct SubPath {
+    start_state: State,
+    transition: Transition,
 }
 
 // pub fn preliminary_check_succes(take some input) -> return a path{
 //    It returns a path
 // }
 
-pub fn is_reachable(begin_state: Option<State>, end_state: State, system: &dyn TransitionSystem) -> Option<Vec<SubPath>>
-{
-    // if preliminary_check_succes() { return a path }  
+pub fn is_reachable(
+    begin_state: Option<State>,
+    end_state: State,
+    system: &dyn TransitionSystem,
+) -> Option<Vec<SubPath>> {
+    // if preliminary_check_succes() { return a path }
 
-    let start_state:State;
+    let start_state: State;
 
-    if begin_state.is_some(){
+    if begin_state.is_some() {
         start_state = begin_state.unwrap();
-    }
-    else if system.get_initial_state().is_some(){
-        start_state =  system.get_initial_state().unwrap();
-    }
-    else{
+    } else if system.get_initial_state().is_some() {
+        start_state = system.get_initial_state().unwrap();
+    } else {
         panic!("No state to start with");
     }
-
 
     searchAlgorithm(start_state, end_state, system)
 }
 
-
-pub fn searchAlgorithm(start_state: State, end_state: State, system: &dyn TransitionSystem) -> Option<Vec<SubPath>>{
+pub fn searchAlgorithm(
+    start_state: State,
+    end_state: State,
+    system: &dyn TransitionSystem,
+) -> Option<Vec<SubPath>> {
     panic!("No implmentation");
 }
-
-
