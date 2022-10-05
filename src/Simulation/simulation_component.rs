408ba46a619804e0adddffcad8fbe42e28bd10ec
pberte20 <71381584+pberte20@users.noreply.github.com>
2022-10-05 10:57:10 +0200

    added test to simulation component

--- src/Simulation/simulation_component.rs
+++ src/Simulation/simulation_component.rs
@@ -11,7 +11,8 @@ pub struct SimulationComponent {
     location: Location,
 }
 
-// Takes a path string & component name, and returns a SimulationComponent.
+// Takes a path string & component name, creates a component, converts the component into a SimulationComponent.
+// Tested [X], Not tested []
 pub fn start_simulation(project_path: &str, component_name: &str) -> SimulationComponent {
     let start_component: Component = json_reader::read_json_component(project_path, component_name);
     let sim_component: SimulationComponent = build_simulation_component(start_component);
@@ -25,6 +26,8 @@ pub fn continue_simulation(simulation_component: SimulationComponent, action: Ch
     simulation_component
 }
 
+//
+// Tested [X], Not tested []
 fn build_simulation_component(component: Component) -> SimulationComponent {
     let t_actions: Vec<Channel> = component.get_actions();
     let t_location: Location = match component.get_initial_location()
@@ -39,6 +42,68 @@ fn build_simulation_component(component: Component) -> SimulationComponent {
     }
 }
 
+
+
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::ModelObjects::component::{
+        Component, DeclarationProvider, Declarations, State, Transition, Channel, Location,
+    };
+    use crate::DataReader::json_reader;
+    use crate::TransitionSystems::{TransitionSystem, TransitionSystemPtr};
+    use crate::Simulation::simulation_component;
+
+    
+    #[test]
+    fn Convert_GivenJSONComponent_ReturnsSimulationComponent() {
+
+        // Arrange
+        let should_equal: Component = json_reader::read_json_component("samples/json/AG", "A");
+
+        // Act
+        let output: simulation_component::SimulationComponent = simulation_component::start_simulation("samples/json/AG", "A");
+
+        // Assert
+        assert_eq!(should_equal, output.component);
+    }
+
+    #[test]
+    fn JsonObject_NotEqualTo_Simulation_Component()
+    {
+        // Arrange
+        let should_equal: Component = json_reader::read_json_component("samples/json/AG", "A");
+
+        // Act
+        let output: simulation_component::SimulationComponent = simulation_component::start_simulation("samples/json/AG", "AA");
+
+        // Assert
+        assert_ne!(should_equal, output.component);
+    }
+
+    #[test]
+    fn Moved_To_New_Location()
+    {
+        // Arrange
+        let test_component: Component = json_reader::read_json_component("samples/json/AG", "Imp");
+        let should_equal: Location = test_component.get_location_by_name("L1").clone();
+        let t_struct: Channel = Channel {
+            name: String::from("t_struct"),
+        };
+        
+        // Act
+        let test_simulation_component: simulation_component::SimulationComponent = simulation_component::start_simulation("samples/json/AG", "Imp");
+        let output: simulation_component::SimulationComponent = simulation_component::continue_simulation(test_simulation_component, t_struct);
+
+
+        // Assert
+        assert_eq!(should_equal, output.location);
+    }
+}
 
 
 
