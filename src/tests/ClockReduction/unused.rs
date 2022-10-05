0fed6ff244ce15921f68fc03bf8b233d37df273a
alexsteffensen <asteff20@student.aau.dk>
2022-10-05 10:10:44 +0200

    setup test

--- src/tests/ClockReduction/unused.rs
+++ src/tests/ClockReduction/unused.rs
@@ -1,9 +1,28 @@
 
 #[cfg(test)]
-mod tests {
+mod unused_clocks_tests {
+    use generic_array::typenum::assert_type_eq;
+    use crate::DataReader::component_loader::JsonProjectLoader;
+
+    static UNUSED_WITH_CYCLE: &str = "samples/json/ClockReductionTest/UnusedClockWithCycle";
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn found_unused_clocks_with_cycles() {
+        let project_loader = JsonProjectLoader::new(String::from(UNUSED_WITH_CYCLE));
+        let component = project_loader.to_comp_loader();
+
+        let unused_clocks = check_for_unused_clocks(component);
+
+        assert!(unused_clocks.get(0).clock, "x");
+    }
+
+    #[test]
+    fn found_unused_clock() {
+        let project_loader = JsonProjectLoader::new(String::from(UNUSED_WITH_CYCLE));
+        let component = project_loader.to_comp_loader();
+
+        let unused_clocks = check_cycles(component);
+
+        assert!(unused_clocks.get(0).clock, "x");
     }
 }
