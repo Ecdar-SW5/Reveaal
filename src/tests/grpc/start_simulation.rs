8bdd0acd095fcc00d4a64263fcb1714d370f39b9
Casper Ståhl <cstahl20@student.aau.dk>
2022-10-07 11:28:44 +0200

    Refactor

--- src/tests/grpc/start_simulation.rs
+++ src/tests/grpc/start_simulation.rs
@@ -1,22 +1,18 @@
 #[cfg(test)]
-mod refinements {
+mod start_simulation {
+    use crate::tests::grpc::grpc_helper;
     use crate::ProtobufServer::{
         self,
         services::{self, ecdar_backend_server::EcdarBackend},
     };
-    use std::fs;
     use tonic;
-
-    //static CONJUN: &str = "samples/xml/conjun.xml";
-    static ECDAR_UNI: &str = "samples/json/EcdarUniversity";
 
     #[tokio::test]
     async fn start_simulation__normal_json__respondes_with_correct_state() {
         // Arrange
         let backend = ProtobufServer::ConcreteEcdarBackend::default();
 
-        let component_json =
-            fs::read_to_string(format!("{}/Components/Machine.json", ECDAR_UNI)).unwrap();
+        let component_json = grpc_helper::create_sample_json_component();
 
         let request: tonic::Request<services::SimulationStartRequest> =
             tonic::Request::new(services::SimulationStartRequest {
@@ -29,55 +25,8 @@ mod refinements {
                 }),
             });
 
-        // the expected respones is the same component (no composition takes place), and the
-        // decision point drawn below:
-        //
-        //             ----coin?---->
-        //            /
-        // <L5,y>=0>-------tea!----->
-        //
-        //
         let expected_response = services::SimulationStepResponse {
-            new_state: Some(services::SimulationState {
-                component: Some(services::Component {
-                    rep: Some(services::component::Rep::Json(component_json.clone())),
-                }),
-                decision_points: vec![services::DecisionPoint {
-                    source: Some(services::State {
-                        location_id: "L5".to_string(),
-                        zone: Some(services::Zone {
-                            disjunction: Some(services::Disjunction {
-                                conjunctions: vec![services::Conjunction {
-                                    constraints: vec![
-                                        services::Constraint {
-                                            x: Some(services::ComponentClock {
-                                                specific_component: None,
-                                                clock_name: "0".to_string(),
-                                            }),
-                                            y: Some(services::ComponentClock {
-                                                specific_component: None,
-                                                clock_name: "y".to_string(),
-                                            }),
-                                            strict: false,
-                                            c: 0,
-                                        }, // constraint (0 - y <= 0) <= (y >= 0)
-                                    ],
-                                }],
-                            }),
-                        }),
-                    }),
-                    edges: vec![
-                        services::Edge {
-                            id: "E3".to_string(),
-                            specific_component: None,
-                        },
-                        services::Edge {
-                            id: "E5".to_string(),
-                            specific_component: None,
-                        },
-                    ],
-                }],
-            }),
+            new_state: Some(grpc_helper::create_sample_state_1()),
         };
 
         // Act
