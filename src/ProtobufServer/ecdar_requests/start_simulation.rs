3ca21904cc399d07e63d2ca46a1a63c251473d4c
Casper Ståhl <cstahl20@student.aau.dk>
2022-10-07 21:02:10 +0200

    Start work on handle_start_simulation

--- src/ProtobufServer/ecdar_requests/start_simulation.rs
+++ src/ProtobufServer/ecdar_requests/start_simulation.rs
@@ -1,19 +1,8 @@
 use std::panic::AssertUnwindSafe;
 
-use core::cell::RefCell;
-use crate::System::input_enabler;
 use crate::DataReader::component_loader::ComponentContainer;
-use crate::DataReader::parse_edge::Rule::bool;
-use crate::DataReader::component_loader::ComponentLoader;
-use crate::DataReader::json_reader::json_to_component;
-use crate::xml_parser::parse_xml_from_str;
-use crate::DataReader::json_writer::component_to_json;
 use crate::ProtobufServer::services::SimulationStepResponse;
-use crate::ProtobufServer::services::component::Rep;
-use crate::ProtobufServer::services::{Component as ProtobufComponent, SimulationStartRequest, Constraint, ComponentClock, ComponentsInfo, SpecificComponent};
-use crate::component::Component;
-use crate::System::extract_system_rep;
-use std::collections::HashMap;
+use crate::ProtobufServer::services::SimulationStartRequest;
 use log::trace;
 use tonic::{Request, Response, Status};
 
@@ -26,15 +15,11 @@ impl ConcreteEcdarBackend {
     ) -> Result<Response<SimulationStepResponse>, Status> {
         trace!("Recieved query: {:?}", request);
 
-        let start_simulation_request = request.0.into_inner();
+        let request_message = request.0.into_inner();
+        let component_info = request_message.components_info.unwrap();
 
-        // Extract the individual components
-        // let components_info = start_simulation_request.components_info;
-        // let components = Vec::new();
-        // for json_component in components_info.components {
-        //     let component = parse_json_component(json_component);
-        //     components.push(component);
-        // }
+        let component_container= 
+            ComponentContainer::from(&component_info);
 
         // Combine components as specified in the composition string
         
