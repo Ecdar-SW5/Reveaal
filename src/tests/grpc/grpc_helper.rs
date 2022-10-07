c12b56390b521b472229146f5467ab41b0a8cf12
Casper Ståhl <cstahl20@student.aau.dk>
2022-10-07 11:27:20 +0200

    Create grpc_helper

---
+++ src/tests/grpc/grpc_helper.rs
@@ -0,0 +1,62 @@
+
+use crate::ProtobufServer::services;
+use std::fs;
+
+static ECDAR_UNI: &str = "samples/json/EcdarUniversity";
+
+// Create a simulation state with the Machine component and the decision point drawn below:
+//
+//             ----coin?---->
+//            /
+// <L5,y>=0>-------tea!----->
+//
+//
+pub fn create_sample_state_1() -> services::SimulationState {
+    let component_json = create_sample_json_component();
+
+    services::SimulationState {
+        component: Some(services::Component {
+            rep: Some(services::component::Rep::Json(component_json.clone())),
+        }),
+        decision_points: vec![services::DecisionPoint {
+            source: Some(services::State {
+                location_id: "L5".to_string(),
+                zone: Some(services::Zone {
+                    disjunction: Some(services::Disjunction {
+                        conjunctions: vec![services::Conjunction {
+                            constraints: vec![
+                                services::Constraint {
+                                    x: Some(services::ComponentClock {
+                                        specific_component: None,
+                                        clock_name: "0".to_string(),
+                                    }),
+                                    y: Some(services::ComponentClock {
+                                        specific_component: None,
+                                        clock_name: "y".to_string(),
+                                    }),
+                                    strict: false,
+                                    c: 0,
+                                }, // constraint (0 - y <= 0) <= (y >= 0)
+                            ],
+                        }],
+                    }),
+                }),
+            }),
+            edges: vec![
+                services::Edge {
+                    id: "E3".to_string(),
+                    specific_component: None,
+                },
+                services::Edge {
+                    id: "E5".to_string(),
+                    specific_component: None,
+                },
+            ],
+        }],
+    }
+}
+
+// Returns the Machine component as a String, in the .json format
+pub fn create_sample_json_component() -> String {
+    fs::read_to_string(format!("{}/Components/Machine.json", ECDAR_UNI)).unwrap()
+}
