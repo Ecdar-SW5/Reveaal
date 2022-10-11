5d13e07aaf486f562960fe725dcb0e5a59d283c8
Darkros1245 <madsmario@gmail.com>
2022-10-11 13:59:32 +0200

    Test
    Added a test, to see if it would return false, if two states are not reachable, and removed some test because they were wrong

--- src/tests/reachability/search_algorithm_test.rs
+++ src/tests/reachability/search_algorithm_test.rs
@@ -5,25 +5,25 @@ mod search_algorithm_test{
     use crate::QueryResult;
     
 
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y<=6)"; "T_Test1")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y>7); [L4](y<=6)"; "T_Test2")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y>=4)"; "T_Test3")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y<4)"; "T_Test4")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<1); [L5](y<2)"; "T_Test5")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine || Researcher -> [L5, L6](); [L4, L9]()"; "T_Test6")]
-    fn reachability_test_search_algorithm_return_true(path: &str, query: &str) {
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y<=6)", true; "T_Test1")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y>7); [L4](y<=6)", true; "T_Test2")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y>=4)", true; "T_Test3")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y<4)", true; "T_Test4")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<1); [L5](y<2)", true; "T_Test5")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine || Researcher -> [L5, L6](); [L4, L9]()", true; "T_Test6")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Researcher -> [U0](); [L7]()", false; "T_Test7")] // IT FAILS HERE.... SADGE
+    fn reachability_test_search_algorithm_returns_result(path: &str, query: &str, expected: bool) {
         match json_run_query(path, query) {
-          QueryResult::Reachability(b, _) => assert!(b), 
+          QueryResult::Reachability(b, _) => assert_eq!(b, expected), 
           QueryResult::Error(e) => panic!("{}", e),
           _ => panic!("Inconsistent query result, expected Reachability")
         }
     }
+
     #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y>7)"; "P_Test1")]
     #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L3](y<6); [L4](y<=6)"; "P_Test2")]
     #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L3](y<=6)"; "P_Test3")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Reseacher -> [L9](x<2); [L7](x<=15)"; "P_Test4")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Reseacher -> [U0](); [L7](x<=15)"; "P_Test5")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine || Researcher -> [L5, L4](); [L6, L9]()"; "P_Test6")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine || Researcher -> [L5, L4](); [L6, L9]()"; "P_Test4")]
     #[should_panic]
     fn reachability_test_search_algorithm_panics(path: &str, query: &str) {
         match json_run_query(path, query) {
