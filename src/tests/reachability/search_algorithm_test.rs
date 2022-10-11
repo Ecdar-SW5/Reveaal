d6db3f16030d07802697a26f530936165e2ccc20
S3JER <72788875+S3JER@users.noreply.github.com>
2022-10-11 10:55:29 +0200

    Rename Test cases, added new testcase
    Two testcases should not panic in returns true test

--- src/tests/reachability/search_algorithm_test.rs
+++ src/tests/reachability/search_algorithm_test.rs
@@ -5,9 +5,10 @@ mod search_algorithm_test{
     use crate::QueryResult;
     
 
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y<=6)")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y>7); [L4](y<=6)")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y>=4)")]//This one makes an error but should work.
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y<=6)"; "Test1")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y>7); [L4](y<=6)"; "Test2")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y>=4)"; "Test3")]//This one makes an error but should work.
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y<4)"; "Test4")] //This one makes an error but should work.
     fn reachability_test_search_algorithme_return_true(path: &str, query: &str) {
         match json_run_query(path, query) {
           QueryResult::Reachability(b, _) => assert!(b), 
@@ -15,10 +16,11 @@ mod search_algorithm_test{
           _ => panic!("Inconsistent query result, expected Reachability")
         }
     }
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y>7)")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L3](y<6); [L4](y<=6)")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L3](y<=6)")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<1); [L5](y<2)")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y>7)"; "Test1")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L3](y<6); [L4](y<=6)"; "Test2")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L3](y<=6)"; "Test3")]
+    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<1); [L5](y<2)"; "Test4")]
+    
     #[should_panic]
     fn reachability_test_search_algorithme_panics(path: &str, query: &str) {
         match json_run_query(path, query) {
