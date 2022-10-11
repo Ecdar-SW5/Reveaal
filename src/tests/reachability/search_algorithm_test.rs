6482159f6c3692d419a09bfe77525f4f08cf98e9
S3JER <72788875+S3JER@users.noreply.github.com>
2022-10-11 14:05:55 +0200

    Made path const

--- src/tests/reachability/search_algorithm_test.rs
+++ src/tests/reachability/search_algorithm_test.rs
@@ -3,15 +3,15 @@ mod search_algorithm_test{
     use test_case::test_case;
     use crate::tests::refinement::Helper::json_run_query;
     use crate::QueryResult;
-    
+    const PATH: &str = "samples/json/EcdarUniversity";
 
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y<=6)", true; "T_Test1")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y>7); [L4](y<=6)", true; "T_Test2")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y>=4)", true; "T_Test3")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L4](y<=6); [L5](y<4)", true; "T_Test4")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<1); [L5](y<2)", true; "T_Test5")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine || Researcher -> [L5, L6](); [L4, L9]()", true; "T_Test6")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Researcher -> [U0](); [L7]()", false; "T_Test7")] // IT FAILS HERE.... SADGE
+    #[test_case(PATH, "reachability: Machine -> [L5](y<6); [L4](y<=6)", true; "T_Test1")]
+    #[test_case(PATH, "reachability: Machine -> [L5](y>7); [L4](y<=6)", true; "T_Test2")]
+    #[test_case(PATH, "reachability: Machine -> [L4](y<=6); [L5](y>=4)", true; "T_Test3")]
+    #[test_case(PATH, "reachability: Machine -> [L4](y<=6); [L5](y<4)", true; "T_Test4")]
+    #[test_case(PATH, "reachability: Machine -> [L5](y<1); [L5](y<2)", true; "T_Test5")]
+    #[test_case(PATH, "reachability: Machine || Researcher -> [L5, L6](); [L4, L9]()", true; "T_Test6")]
+    #[test_case(PATH, "reachability: Researcher -> [U0](); [L7]()", false; "T_Test7")] // IT FAILS HERE.... SADGE
     fn reachability_test_search_algorithm_returns_result(path: &str, query: &str, expected: bool) {
         match json_run_query(path, query) {
           QueryResult::Reachability(b, _) => assert_eq!(b, expected), 
@@ -20,10 +20,10 @@ mod search_algorithm_test{
         }
     }
 
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L4](y>7)"; "P_Test1")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L3](y<6); [L4](y<=6)"; "P_Test2")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine -> [L5](y<6); [L3](y<=6)"; "P_Test3")]
-    #[test_case("samples/json/EcdarUniversity", "reachability: Machine || Researcher -> [L5, L4](); [L6, L9]()"; "P_Test4")]
+    #[test_case(PATH, "reachability: Machine -> [L5](y<6); [L4](y>7)"; "P_Test1")]
+    #[test_case(PATH, "reachability: Machine -> [L3](y<6); [L4](y<=6)"; "P_Test2")]
+    #[test_case(PATH, "reachability: Machine -> [L5](y<6); [L3](y<=6)"; "P_Test3")]
+    #[test_case(PATH, "reachability: Machine || Researcher -> [L5, L4](); [L6, L9]()"; "P_Test4")]
     #[should_panic]
     fn reachability_test_search_algorithm_panics(path: &str, query: &str) {
         match json_run_query(path, query) {
