b2d7563811991551b58a28b9b20fa39fd744b32c
pberte20 <71381584+pberte20@users.noreply.github.com>
2022-10-11 14:54:14 +0200

    Created start simulation bad json responds with error

--- src/tests/grpc/start_simulation.rs
+++ src/tests/grpc/start_simulation.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod start_simulation {
+    use std::result;
+
     use crate::tests::grpc::grpc_helper;
     use crate::ProtobufServer::{
         self,
@@ -37,5 +39,41 @@ mod start_simulation {
             .into_inner();
         // Assert
         assert_eq!(actual_response, expected_response);
+    }
+
+    #[tokio::test]
+    async fn start_simulation_bad_json_responds_with_error() {
+        
+        // Arrange
+        let backend = ProtobufServer::ConcreteEcdarBackend::default();
+        let test_json: String = String::from("this is an invalid argument");
+
+        let request: tonic::Request<services::SimulationStartRequest> =
+        tonic::Request::new(services::SimulationStartRequest {
+            component_composition: String::from("Machine"),
+            components_info: Some(services::ComponentsInfo {
+                components: vec![services::Component {
+                    rep: Some(services::component::Rep::Json(test_json.clone())),
+                }],
+                components_hash: 0, // TODO: this is not correct, but will do for now
+            }),
+        });
+
+        let expected_response = tonic::Status::invalid_argument("Bad JSON");
+
+        // Act
+        let actual_response = backend
+            .start_simulation(request)
+            .await
+            .err()
+            .unwrap()
+            .code();
+
+        // Assert
+        
+        assert_eq!(
+            actual_response,
+            expected_response.code()
+        )
     }
 }
