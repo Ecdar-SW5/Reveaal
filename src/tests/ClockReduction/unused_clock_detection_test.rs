b254199d1a3754d5371dea3f0c6e9c85cf58a0af
alexsteffensen <asteff20@student.aau.dk>
2022-10-12 15:23:45 +0200

    Added unused_clock_test functionality to helper

--- src/tests/ClockReduction/unused_clock_detection_test.rs
+++ src/tests/ClockReduction/unused_clock_detection_test.rs
@@ -1,31 +1,17 @@
 
 #[cfg(test)]
 mod unused_clocks_tests {
-    use std::collections::HashSet;
+    use std::collections::{HashMap, HashSet};
     use crate::component::RedundantClock;
     use crate::DataReader::json_reader::read_json_component;
-
-    fn assert_unused_clocks_detection(unused_clocks: &Vec<RedundantClock>, expected_clocks: HashSet<&str> ,expected_amount: usize) {
-
-        let mut clocks_reduced : HashSet<String> = HashSet::new();
-
-        for unused_clock in unused_clocks {
-            assert!(expected_clocks.contains(unused_clock.clock.as_str()), "Clock ({}) is not set as unused, but is not in expected", unused_clock.clock
-                .as_str());
-            assert!(!clocks_reduced.contains(&unused_clock.clock), "Clock {} has been removed multiple times", unused_clock.clock);
-
-            clocks_reduced.insert(unused_clock.clock.clone());
-
-        }
-        assert_eq!(expected_amount, unused_clocks.len(), "{} clocks are unused, expected {} clocks", unused_clocks.len(), expected_amount);
-    }
+    use crate::tests::ClockReduction::helper::test::{assert_duplicated_clock_detection};
 
     fn unused_clocks_with_cycles(component_name: &str, unused_clock: &str) {
         let component = read_json_component("samples/json/ClockReductionTest/UnusedClockWithCycle", component_name);
 
         let unused_clocks = component.find_redundant_clocks();
 
-        assert_unused_clocks_detection(&unused_clocks, HashSet::from([unused_clock]), 1);
+        assert_duplicated_clock_detection(&unused_clocks, 1, HashSet::from([unused_clock]), true);
     }
 
     fn unused_clock(component_name: &str, unused_clock: &str) {
@@ -33,7 +19,7 @@ mod unused_clocks_tests {
 
         let unused_clocks = component.find_redundant_clocks();
 
-        assert_unused_clocks_detection(&unused_clocks, HashSet::from([unused_clock]), 1);
+        assert_duplicated_clock_detection(&unused_clocks,1, HashSet::from([unused_clock]), true);
     }
 
     #[test]
@@ -45,6 +31,4 @@ mod unused_clocks_tests {
         unused_clock("Component2", "i");
         unused_clock("Component3", "c");
     }
-
-
 }
