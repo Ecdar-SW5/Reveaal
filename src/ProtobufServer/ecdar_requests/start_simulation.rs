c51920c31f69732e4da9f8475fd539b86d60d2f5
Mightyhaha <74659365+Mightyhaha@users.noreply.github.com>
2022-10-14 10:21:25 +0200

    Start_simulation added

--- src/ProtobufServer/ecdar_requests/start_simulation.rs
+++ src/ProtobufServer/ecdar_requests/start_simulation.rs
@@ -1,8 +1,10 @@
 use std::panic::AssertUnwindSafe;
 
-use crate::DataReader::component_loader::ComponentContainer;
+use crate::DataReader::component_loader::{ComponentContainer, self};
 
-use crate::ProtobufServer::services::{SimulationStartRequest, SimulationStepResponse};
+use crate::ProtobufServer::services::{SimulationStartRequest, SimulationStepResponse, SimulationState};
+use crate::extract_system_rep::get_system_recipe;
+use crate::parse_queries::parse_to_expression_tree;
 
 use log::trace;
 
@@ -16,23 +18,30 @@ impl ConcreteEcdarBackend {
         request: AssertUnwindSafe<Request<SimulationStartRequest>>,
     ) -> Result<Response<SimulationStepResponse>, Status> {
         trace!("Received query: {:?}", request);
-
+        
         let request_message = request.0.into_inner();
         let component_info = request_message.components_info.unwrap();
+        let composition = request_message.component_composition;
 
         // Extract components from the request message
-        let _component_container = ComponentContainer::from(&component_info);
+        let mut component_container = ComponentContainer::from(&component_info).unwrap();
 
         // Combine components as specified in the composition string
-
-        // let system: TransitionSystemPtr;
-        // let comb = combine_components(system, PruningStrategy::Reachable);
+        let mut dimension = 0;
+        let composition_as_expression_tree = parse_to_expression_tree(&composition);
+        let transition_system = get_system_recipe(&composition_as_expression_tree[0], &mut component_container, &mut dimension, &mut None).compile(dimension);
 
         // Send the combine component to the Simulation module
+        let initial_decision_point = get_initial_decision_from(transition_system);
 
-        // Serialize and respond with the SimulatioState result form the simulation module
+        // Serialize and respond with the SimulationState result from the simulation module
+        let initial_simulation_state = SimulationState{
+            component_composition: composition,
+            components_info: Some(component_info),
+            decision_points: vec![initial_decision_point],
+        };
+        let simulation_step_response = SimulationStepResponse { new_state: Some(initial_simulation_state) };
 
-        // Ok(Response::new(None);
-        Err(Status::unimplemented(""))
+        Ok(Response::new(simulation_step_response))
     }
 }
