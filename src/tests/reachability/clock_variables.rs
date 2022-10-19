2a5e8777d6e3f104551e6aa5693dabb46d654f32
henneboy <72788863+henneboy@users.noreply.github.com>
2022-10-19 13:11:41 +0200

    Clock variable test update

--- src/tests/reachability/clock_variables.rs
+++ src/tests/reachability/clock_variables.rs
@@ -7,15 +7,15 @@ mod reachability_parser_clock_variable_validation {
 
     //These tests checks that the parser only accepts clock variable arguments with existing clock variables.
     // i.e. check that the the variables exist in the model.
-    #[test_case("reachability: Adm2 -> [L21](x>1); [L20]()";
+    #[test_case("reachability: Adm2 -> [L21](u>1); [L20]()";
     "The clock variable u in start state does not exist in the model")]
-    // #[test_case("reachability: Adm2 -> [L21](); [L20](u>1)";
-    // "The clock variable u in end state does not exist in the model")]
+    #[test_case("reachability: Adm2 -> [L21](uwu>1); [L20]()";
+    "The clock variable uwu in start state does not exist in the model")]
     // #[test_case("reachability: Adm2 -> [L21](u>1); [L20](u>1)";
     // "The clock variable u in start and end state does not exist in the model")]
     // #[test_case("reachability: Adm2 -> [L21](ux>1); [L20]()";
     // "The clock variable ux in start state does not exist in the model")]
-    #[should_panic] //Should return error
+    #[should_panic]
     fn query_parser_checks_invalid_clock_variables(parser_input: &str) {
         let folder_path = "samples/json/EcdarUniversity".to_string();
         let mut comp_loader = if xml_parser::is_xml_project(&folder_path) {
@@ -28,7 +28,7 @@ mod reachability_parser_clock_variable_validation {
         let q = parse_queries::parse_to_query(parser_input);
         let queries = q.first().unwrap();
 
-        // Runs the "validate_reachability" function from extract_system_rep, which we wish to test.
+        // This funciton runs the "get_state" function from extract_state, which we wish to test.
         match extract_system_rep::create_executable_query(queries, &mut *comp_loader) {
             Err(_) => (),
             Ok(_) => panic!("Expected Err, recieved Ok"),
