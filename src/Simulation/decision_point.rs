06dc9ce795fdfa85f6288d53dc481b9efca139ed
Mightyhaha <74659365+Mightyhaha@users.noreply.github.com>
2022-10-25 15:47:01 +0200

    format

--- src/Simulation/decision_point.rs
+++ src/Simulation/decision_point.rs
@@ -1,6 +1,9 @@
-use edbm::util::constraints::ClockIndex;
-use crate::{component::{State, Edge, Transition, Component}, TransitionSystems::{LocationTuple, LocationID}};
 use super::transition_decision::TransitionDecision;
+use crate::{
+    component::{Component, Edge, State, Transition},
+    TransitionSystems::{LocationID, LocationTuple},
+};
+use edbm::util::constraints::ClockIndex;
 
 #[allow(dead_code)]
 #[derive(Clone)]
@@ -11,30 +14,37 @@ pub struct DecisionPoint {
 
 impl DecisionPoint {
     // Creates the new decision point
-    pub fn new(transitionDecision: TransitionDecision, components: Vec<Component>, chosen_state: usize) -> Self {
+    pub fn new(
+        transitionDecision: TransitionDecision,
+        components: Vec<Component>,
+        chosen_state: usize,
+    ) -> Self {
         let source = (transitionDecision.source).clone();
-        let edges = Self::get_edges_with_transitions_for_chosen_component(transitionDecision, components, chosen_state);
-        DecisionPoint { source: source, edges: edges }
+        let edges = Self::get_edges_with_transitions_for_chosen_component(
+            transitionDecision,
+            components,
+            chosen_state,
+        );
+        DecisionPoint {
+            source: source,
+            edges: edges,
+        }
     }
 
     // Allows us to access location tuple to find locations
     pub fn get_location_tuple(transitionDecision: TransitionDecision) -> LocationTuple {
-        
-       let locationTuple = transitionDecision.source.decorated_locations;
-       locationTuple
-        
+        let locationTuple = transitionDecision.source.decorated_locations;
+        locationTuple
     }
 
     // Allows us to access transitions to add to edge ids
     pub fn get_transitions(transitionDecision: TransitionDecision) -> Vec<Transition> {
-        
         let transitions = transitionDecision.transitions;
         transitions
     }
 
     // Get all edges from components
     pub fn get_all_edges_from_components(components: Vec<Component>) -> Vec<Edge> {
-
         let mut all_edges: Vec<Edge> = Vec::new();
         let mut cloned_components: Vec<Component> = Vec::new();
 
@@ -53,8 +63,10 @@ impl DecisionPoint {
     }
 
     // Add transitions to corrospondent edge ID
-    pub fn add_transition_to_edge(transitionDecision: TransitionDecision, components: Vec<Component>) -> Vec<Transition>{
-
+    pub fn add_transition_to_edge(
+        transitionDecision: TransitionDecision,
+        components: Vec<Component>,
+    ) -> Vec<Transition> {
         let transitions = Self::get_transitions(transitionDecision.clone());
         let locationTuple = Self::get_location_tuple(transitionDecision.clone());
         let mut new_transitions: Vec<Transition> = Vec::new();
@@ -69,11 +81,11 @@ impl DecisionPoint {
                 if locationTuple.id == LocationID::from_string(edge.get_target_location()) {
                     // 4. Add transition to the corropsondent edge ID
                     for component in &components {
-                    let transition = Transition::from(&component, &edge, dim);
-                    new_transitions.push(transition);
+                        let transition = Transition::from(&component, &edge, dim);
+                        new_transitions.push(transition);
                     }
                 }
-            }     
+            }
         }
         // 5. Return the new transitions with corropsodent edge ids
         new_transitions
@@ -81,12 +93,10 @@ impl DecisionPoint {
 
     // Get all edges corrospodent to the chosen ID
     pub fn get_edges_with_transitions_for_chosen_component(
-        transitionDecision: TransitionDecision, 
-        components: Vec<Component>, 
-        chosen_state: usize
+        transitionDecision: TransitionDecision,
+        components: Vec<Component>,
+        chosen_state: usize,
     ) -> Vec<Edge> {
-
-
         let chosen_component = components[chosen_state].clone();
         let mut chosen_edges: Vec<Edge> = Vec::new();
         let edges = Component::get_edges(&chosen_component);
@@ -95,7 +105,9 @@ impl DecisionPoint {
 
         for transition in transitions {
             for edge in edges {
-                if transition.target_locations.id == LocationID::from_string(edge.get_target_location()) {
+                if transition.target_locations.id
+                    == LocationID::from_string(edge.get_target_location())
+                {
                     let e = edge.clone();
                     chosen_edges.push(e);
                 }
@@ -109,10 +121,11 @@ impl DecisionPoint {
 
 mod tests {
 
-    use super::{TransitionDecision, DecisionPoint};
+    use super::{DecisionPoint, TransitionDecision};
     use crate::{
+        component::{State, Transition},
         DataReader::json_reader::read_json_component,
-        TransitionSystems::{CompiledComponent, TransitionSystemPtr}, component::{State, Transition},
+        TransitionSystems::{CompiledComponent, TransitionSystemPtr},
     };
 
     fn create_EcdarUniversity_Machine_system() -> TransitionSystemPtr {
@@ -121,7 +134,7 @@ mod tests {
         CompiledComponent::from(vec![component], "Machine")
     }
     // get_transitions test
-#[test]
+    #[test]
     fn get_transitions_CorrectTransitionsReturned_ReturnsVectorOfTransitions() {
         // arrange
         let system = create_EcdarUniversity_Machine_system();
@@ -142,6 +155,3 @@ mod tests {
     // add_transition_to_edge test
     // get_edges_with_transitions_for_chosen_component test
 }
-
-
-
--- src/Simulation/mod.rs
+++ src/Simulation/mod.rs
@@ -1,3 +1,3 @@
+pub mod decision_point;
 pub mod graph_layout;
 pub mod transition_decision;
-pub mod decision_point;
