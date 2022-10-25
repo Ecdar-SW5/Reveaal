cc963dbbc98301d32e2c61a39e048558dbfc4116
Simon Andersen <smpandersen@gmail.com>
2022-10-25 10:32:21 +0200

    COmmit before trying to update submodule.

--- src/DataReader/decisionpoint_serializer.rs
+++ src/DataReader/decisionpoint_serializer.rs
@@ -6,6 +6,16 @@ use crate::ModelObjects::state::State;
 pub struct SerializedDecisionPoint {}
 
 impl SerializedDecisionPoint {}
+pub mod util {
+    use crate::DataReader::component_loader::JsonProjectLoader;
+    use crate::DataReader::parse_queries;
+    use crate::ModelObjects::representations::QueryExpression;
+    use crate::System::extract_system_rep;
+    use crate::System::extract_system_rep::SystemRecipe;
+    use crate::System::refine;
+    use crate::System::save_component::combine_components;
+    use crate::System::save_component::PruningStrategy;
+    use edbm::util::constraints::ClockIndex;
 
 #[cfg(test)]
 mod tests {
@@ -13,10 +23,55 @@ mod tests {
         testSource: State,
         testEdges: Vec<Edge>,
     }
+    
+    pub fn setupHelper(input_path: &str, system: &str) -> Setup {
+        let project_loader = JsonProjectLoader::new(String::from(input_path));
+
+        //This query is not executed but simply used to extract an UncachedSystem so the tests can just give system expressions
+        let str_query = format!("get-component: {} save-as test", system);
+        let query = parse_queries::parse_to_expression_tree(str_query.as_str()).remove(0);
+
+        let mut dim: ClockIndex = 0;
+        let (base_system, new_system) = if let QueryExpression::GetComponent(expr) = &query {
+            let mut comp_loader = project_loader.to_comp_loader();
+            (
+                extract_system_rep::get_system_recipe(
+                    expr.as_ref(),
+                    &mut *comp_loader,
+                    &mut dim,
+                    &mut None,
+                ),
+                extract_system_rep::get_system_recipe(
+                    expr.as_ref(),
+                    &mut *comp_loader,
+                    &mut dim,
+                    &mut None,
+                ),
+            )
+        } else {
+            panic!("Failed to create system")
+        };
+
+        let new_comp = new_system.compile(dim);
+        if new_comp.is_err() {
+            return;
+        }
+
+        let new_comp = combine_components(&new_comp.unwrap(), PruningStrategy::NoPruning);
+
+        let new_comp = SystemRecipe::Component(Box::new(new_comp))
+            .compile(dim)
+            .unwrap();
+        let base_system = base_system.compile(dim).unwrap();
+        Setup {
+            State: base_system.get_initial_state,
+            testEdges: base_system.get_ed
+        }
+    }
 
     impl Setup {
         fn new() -> Self {
-            testSource = State::create();
+            testSource = State;
             testEdges = vec![]
         }
     }
