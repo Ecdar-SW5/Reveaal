72aa580f78138ee2edb0bbfb6c9d1ab545b0496a
S3JER <72788875+S3JER@users.noreply.github.com>
2022-11-01 11:37:14 +0100

    Working test for checking edge ids on a component
    Co-Authored-By: Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>

--- src/tests/reachability/edge_tests.rs
+++ src/tests/reachability/edge_tests.rs
@@ -8,12 +8,8 @@ mod edge_tests {
     #[test_case(FOLDER_PATH, "Machine", vec!["E25".to_string(), "E26".to_string(), "E27".to_string(), "E28".to_string(), "E29".to_string()]; "Edge ID test on Machine from the ECDAR University")]
     fn edge_id_checking(path: &str, component_name: &str, edge_ids: Vec<String>) {
         let component = read_json_component(path, component_name);
-        for i in [0..component.edges.len()] {
-            assert_eq!((&component.edges[i]).id, edge_ids[i])
-        }
-
-        for edge in component.edges {
-            assert!(edge_ids.contains(&edge.id));
+        for i in 0..component.edges.len() {
+            assert_eq!(component.edges[i].id, edge_ids[i])
         }
     }
 }
