115a62a6da607eab8f9b8849e1f7ec51b739e855
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2022-11-01 13:15:31 +0100

    Added TransitionID which mostly a copy of LocationID

---
+++ src/TransitionSystems/transition_id.rs
@@ -0,0 +1,58 @@
+use std::fmt::{Display, Formatter};
+
+use crate::ModelObjects::representations::QueryExpression;
+
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
+pub enum TransitionID {
+    Conjunction(Box<TransitionID>, Box<TransitionID>),
+    Composition(Box<TransitionID>, Box<TransitionID>),
+    Quotient(Box<TransitionID>, Box<TransitionID>),
+    Simple(String),
+}
+
+impl Display for TransitionID {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TransitionID::Conjunction(left, right) => {
+                match *(*left) {
+                    TransitionID::Conjunction(_, _) => write!(f, "{}", (*left))?,
+                    TransitionID::Simple(_) => write!(f, "{}", (*left))?,
+                    _ => write!(f, "({})", (*left))?,
+                };
+                write!(f, "&&")?;
+                match *(*right) {
+                    TransitionID::Conjunction(_, _) => write!(f, "{}", (*right))?,
+                    TransitionID::Simple(_) => write!(f, "{}", (*right))?,
+                    _ => write!(f, "({})", (*right))?,
+                };
+            }
+            TransitionID::Composition(left, right) => {
+                match *(*left) {
+                    TransitionID::Composition(_, _) => write!(f, "{}", (*left))?,
+                    TransitionID::Simple(_) => write!(f, "{}", (*left))?,
+                    _ => write!(f, "({})", (*left))?,
+                };
+                write!(f, "||")?;
+                match *(*right) {
+                    TransitionID::Composition(_, _) => write!(f, "{}", (*right))?,
+                    TransitionID::Simple(_) => write!(f, "{}", (*right))?,
+                    _ => write!(f, "({})", (*right))?,
+                };
+            }
+            TransitionID::Quotient(left, right) => {
+                match *(*left) {
+                    TransitionID::Simple(_) => write!(f, "{}", (*left))?,
+                    _ => write!(f, "({})", (*left))?,
+                };
+                write!(f, "\\\\")?;
+                match *(*right) {
+                    TransitionID::Simple(_) => write!(f, "{}", (*right))?,
+                    _ => write!(f, "({})", (*right))?,
+                };
+            }
+            TransitionID::Simple(name) => write!(f, "{}", name)?,
+            TransitionID::AnyLocation() => write!(f, "_")?,
+        }
+        Ok(())
+    }
+}
