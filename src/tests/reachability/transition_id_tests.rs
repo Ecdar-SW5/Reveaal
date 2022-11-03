6c36f4ffa144efb7fba926358ff557cc2b155ab7
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2022-11-03 10:44:16 +0100

    Finished the transition ID test

--- src/tests/reachability/transition_id_tests.rs
+++ src/tests/reachability/transition_id_tests.rs
@@ -1,5 +1,8 @@
 #[cfg(test)]
 mod reachability_transition_id_test {
+    use std::collections::HashSet;
+    use std::iter::FromIterator;
+
     use crate::DataReader::parse_queries;
     use crate::TransitionSystems::TransitionID;
     use crate::{
@@ -21,14 +24,21 @@ mod reachability_transition_id_test {
         TransitionID::Simple("E29".to_string())])]
     fn transition_id_test(path: &str, machineExpression: QueryExpression, transition_ids: Vec<TransitionID>) {
         let mock_model = Box::new(machineExpression);
+        let mut expected_ids: HashSet<&TransitionID> = HashSet::from_iter(transition_ids.iter());
         let (_, system) =
             reachability_test_helper_functions::create_system_recipe_and_machine(*mock_model, path);
         for loc in system.get_all_locations() {
             for ac in system.get_actions() {
                 for tran in system.next_transitions(&loc, &ac){
-                    tran.id;
+                    if expected_ids.contains(&tran.id){
+                        expected_ids.remove(&tran.id);
+                    }
+                    else{
+                        panic!("Found unexpected ID in transition system: {}", &tran.id)
+                    }
                 }
             }
         }
+        assert_eq!(expected_ids.len(), 0);
     }
 }
