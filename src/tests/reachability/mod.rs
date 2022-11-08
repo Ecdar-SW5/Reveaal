9c5d12f37caff123383e2e3c73bb8f28d6cfbf26
S3JER <72788875+S3JER@users.noreply.github.com>
2022-11-08 13:40:14 +0100

    cargo fmt

--- src/tests/reachability/mod.rs
+++ src/tests/reachability/mod.rs
@@ -5,5 +5,5 @@ pub mod location_validation;
 pub mod parse_partial_state;
 pub mod parser_output_datatypes;
 pub mod parser_restrictions;
-pub mod search_algorithm_test;
 pub mod partial_state;
+pub mod search_algorithm_test;
