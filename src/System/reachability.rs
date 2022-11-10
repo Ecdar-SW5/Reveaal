89a236b5fede8f73a037a8878656dbd0839554c5
henneboy <72788863+henneboy@users.noreply.github.com>
2022-11-10 12:08:21 +0100

    Cargo fmt

--- src/System/reachability.rs
+++ src/System/reachability.rs
@@ -3,8 +3,8 @@ use edbm::zones::OwnedFederation;
 use crate::ModelObjects::component::{State, Transition};
 use crate::TransitionSystems::{LocationID, TransitionSystem};
 use std::collections::HashMap;
-use std::rc::Rc;
 use std::process::id;
+use std::rc::Rc;
 
 pub struct Path {
     pub path: Option<Vec<Transition>>,
@@ -162,7 +162,9 @@ fn search_algorithm(
         }
 
         for action in &actions {
-            for transition in &system.next_transitions(&sub_path.destination_state.decorated_locations, action) {
+            for transition in
+                &system.next_transitions(&sub_path.destination_state.decorated_locations, action)
+            {
                 take_transition(
                     &sub_path,
                     transition,
@@ -194,7 +196,7 @@ fn take_transition(
     frontier_states: &mut Vec<Rc<SubPath>>,
     visited_states: &mut HashMap<LocationID, Vec<OwnedFederation>>,
     system: &dyn TransitionSystem,
-){
+) {
     let mut new_state = sub_path.destination_state.clone();
     if transition.use_transition(&mut new_state) {
         new_state.extrapolate_max_bounds(system); // Do we need to do this? consistency check does this
