b53d2f175e7cac62acdaabc182ea4216b59d37e5
Mikkel-Boje <milars20@student.aau.dk>
2022-11-10 14:43:45 +0100

    Fixed clippy

--- src/DataReader/mod.rs
+++ src/DataReader/mod.rs
@@ -3,6 +3,8 @@ pub mod json_reader;
 pub mod json_writer;
 pub mod parse_edge;
 pub mod parse_invariant;
-pub mod parse_queries;
 pub mod serialization;
 pub mod xml_parser;
+#[allow(deprecated)]
+pub mod parse_queries;
+
