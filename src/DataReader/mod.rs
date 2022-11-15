08b526bb7b1b1d52946cab12f63d01a4353d1370
Mikkel-Boje <milars20@student.aau.dk>
2022-11-15 10:59:41 +0100

    Allowed the deprecated parser to remove the 15 clippy warnings

--- src/DataReader/mod.rs
+++ src/DataReader/mod.rs
@@ -3,6 +3,7 @@ pub mod json_reader;
 pub mod json_writer;
 pub mod parse_edge;
 pub mod parse_invariant;
-pub mod parse_queries;
 pub mod serialization;
 pub mod xml_parser;
+#[allow(deprecated)]
+pub mod parse_queries;
