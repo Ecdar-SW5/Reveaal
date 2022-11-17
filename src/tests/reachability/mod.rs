6ad6b295affa667e7fb0d0bdaf8b6b24f6af4da0
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2022-11-17 10:46:03 +0100

    Added tests for split component

--- src/tests/reachability/mod.rs
+++ src/tests/reachability/mod.rs
@@ -9,3 +9,4 @@ pub mod parser_output_datatypes;
 pub mod parser_restrictions;
 pub mod partial_state;
 pub mod search_algorithm_test;
+pub mod split_component_tests;
---
+++ src/tests/reachability/split_component_tests.rs
@@ -0,0 +1,112 @@
+#[cfg(test)]
+mod reachability_search_algorithm_test {
+    use crate::TransitionSystems::TransitionID;
+
+    use test_case::test_case;
+    #[test_case(
+        vec![
+        ],
+        vec![
+
+        ]
+        ;
+    "Empty path")]
+    #[test_case(
+        vec![
+            TransitionID::Simple("a".to_string())
+        ],
+        vec![
+            // component 1
+            vec![
+                // transition 1
+                vec![TransitionID::Simple("a".to_string())]
+            ]
+        ]
+        ;
+    "Simplest path")]
+    #[test_case(
+        vec![
+            TransitionID::Simple("a".to_string()),
+            TransitionID::None
+
+        ],
+        vec![
+            // component 1
+            vec![
+                // transition 1
+                vec![TransitionID::Simple("a".to_string())],
+                vec![]
+            ]
+        ]
+        ;
+    "Has none")]
+
+    #[test_case(
+        vec![
+            TransitionID::Conjunction(
+                Box::new(TransitionID::Simple("a".to_string())),
+                Box::new(TransitionID::Simple("b".to_string()))
+            )
+        ],
+        vec![
+            // component 1
+            vec![
+                // transition 1
+                vec![TransitionID::Simple("a".to_string())]
+            ],
+            // component 2
+            vec![
+                vec![TransitionID::Simple("b".to_string())]
+            ]
+        ]
+        ;
+    "One conjunction")]
+    #[test_case(
+        vec![
+            TransitionID::Conjunction(
+                Box::new(TransitionID::Simple("a".to_string())),
+                Box::new(TransitionID::Simple("b".to_string()))
+            ),
+            TransitionID::Conjunction(
+                Box::new(TransitionID::Simple("c".to_string())),
+                Box::new(TransitionID::Simple("d".to_string()))
+            ),
+            TransitionID::Conjunction(
+                Box::new(TransitionID::Simple("e".to_string())),
+                Box::new(TransitionID::Simple("f".to_string()))
+            )
+        ],
+        vec![
+            // component 1
+            vec![
+                // transition 1
+                vec![TransitionID::Simple("a".to_string())],
+                vec![TransitionID::Simple("c".to_string())],
+                vec![TransitionID::Simple("e".to_string())]
+            ],
+            // component 2
+            vec![
+                vec![TransitionID::Simple("b".to_string())],
+                vec![TransitionID::Simple("d".to_string())],
+                vec![TransitionID::Simple("f".to_string())]
+            ]
+        ]
+        ;
+    "Path")]
+    fn split_component_test(path: Vec<TransitionID>, expected: Vec<Vec<Vec<TransitionID>>>) {
+        assert_eq!(TransitionID::split_into_component_lists(&path), expected);
+        //assert_eq!(id.get_leaves(), expected);
+    }
+
+    #[test_case(
+        vec![
+            TransitionID::Simple("a".to_string())
+        ];
+    "Empty path")]
+
+    fn split_component_invalid_input(path: Vec<TransitionID>) {
+        //assert_eq!(TransitionID::split_into_component_lists(&path));
+        //assert_eq!(id.get_leaves(), expected);
+    }
+
+}
