682665d7ef9411f9d5bfe479cdd828c1ce3317da
Puvikaran2001 <74664443+Puvikaran2001@users.noreply.github.com>
2022-11-24 11:04:55 +0100

    Fix test

--- src/tests/reachability/partial_state.rs
+++ src/tests/reachability/partial_state.rs
@@ -49,53 +49,46 @@ mod reachability_partial_states_test {
         assert!(loc1.compare_partial_locations(&loc2));
     }
 
-    // #[test_case(LocationID::Composition(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L5".to_string(), component_id: None })),
-    //             LocationID::Composition(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L1".to_string(), component_id: None }));
-    //             "L2||L5 != L2||L1")]
-    // #[test_case(LocationID::Quotient(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L6".to_string(), component_id: None })),
-    //             LocationID::Composition(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L1".to_string(), component_id: None }));
-    //             "L2//L6 != L2||L1")]
-    // #[test_case(LocationID::Quotient(Box::new(LocationID::Simple { location_id: "L7".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L6".to_string(), component_id: None })),
-    //             LocationID::Conjunction(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L1".to_string(), component_id: None }));
-    //             "L7//L6 != L2&&L1")]
-    // #[test_case(LocationID::Quotient(Box::new(LocationID::Simple { location_id: "L8".to_string(), component_id: None }),Box::new(LocationID::AnyLocation())),
-    //             LocationID::Conjunction(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L1".to_string(), component_id: None }));
-    //             "L8//_ != L2&&L1")]
-    // #[test_case(LocationID::AnyLocation(),
-    //             LocationID::Conjunction(Box::new(LocationID::Simple { location_id: "L6".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L1".to_string(), component_id: None }));
-    //             "_ != L6&&L1")]
-    // #[test_case(LocationID::AnyLocation(),
-    //             LocationID::Conjunction(Box::new(LocationID::AnyLocation()),Box::new(LocationID::AnyLocation()));
-    //             "anylocation _ != _&&_")]
-    // #[test_case(LocationID::Conjunction(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L4".to_string(), component_id: None })),
-    //             LocationID::Quotient(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L4".to_string(), component_id: None }));
-    //             "L2&&L4 != L2\\L4")]
-    // #[test_case(LocationID::Conjunction(Box::new(LocationID::Composition(Box::new(LocationID::AnyLocation()),Box::new(LocationID::AnyLocation()))),Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None })),
-    //             LocationID::Composition(Box::new(LocationID::Composition(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L1".to_string(), component_id: None }))),Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }));
-    //             "_||_&&L2 == L2||L1||L2")]
-    // #[test_case(LocationID::Conjunction(Box::new(LocationID::Composition(Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None }),Box::new(LocationID::AnyLocation()))),Box::new(LocationID::Simple { location_id: "L2".to_string(), component_id: None })),
-    //             LocationID::Conjunction(Box::new(LocationID::AnyLocation()),Box::new(LocationID::AnyLocation()));
-    //             "L2||_&&L2 == _&&_")]
-    // #[test_case(LocationID::Simple { location_id: "L7".to_string(), component_id: None },
-    //             LocationID::Simple { location_id: "L5".to_string(), component_id: None };
-    //             "L7 != L5")]
-    // #[test_case(LocationID::Quotient(Box::new(LocationID::AnyLocation()),Box::new(LocationID::AnyLocation())),
-    //             LocationID::Conjunction(Box::new(LocationID::Simple { location_id: "L6".to_string(), component_id: None }),Box::new(LocationID::Simple { location_id: "L25".to_string(), component_id: None }));
-    //             "_//_ != L6&&L25")]
-    // #[test_case(LocationID::Simple { location_id: "_L1".to_string(), component_id: None },
-    //             LocationID::Simple { location_id: "L1".to_string(), component_id: None };
-    //             "_L1 != L1")]
-    // #[test_case(LocationID::Simple { location_id: "__".to_string(), component_id: None },
-    //             LocationID::Simple { location_id: "L7".to_string(), component_id: None };
-    //             "__ != L7")]
-    // fn checks_cmp_locations_returns_false(loc1: LocationID, loc2: LocationID) {
-    //     assert!(!loc1.compare_partial_locations(&loc2));
-    // }
+    #[test_case(LocationTuple::compose(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L5", LocationType::Normal), CompositionType::Composition),
+                LocationTuple::compose(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L1", LocationType::Normal), CompositionType::Composition);
+                "L2||L5 != L2||L1")]
+    #[test_case(LocationTuple::merge_as_quotient(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L6", LocationType::Normal)),
+                LocationTuple::compose(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L1", LocationType::Normal), CompositionType::Composition);
+                "L2//L6 != L2||L1")]
+    #[test_case(LocationTuple::merge_as_quotient(&build_location_tuple_helper("L7", LocationType::Normal), &build_location_tuple_helper("L6", LocationType::Normal)),
+                LocationTuple::compose(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L1", LocationType::Normal), CompositionType::Conjunction);
+                "L7//L6 != L2&&L1")]
+    #[test_case(LocationTuple::merge_as_quotient(&build_location_tuple_helper("L8", LocationType::Normal), &LocationTuple::build_any_location_tuple()),
+                LocationTuple::compose(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L1", LocationType::Normal), CompositionType::Conjunction);
+                "L8//_ != L2&&L1")]
+    #[test_case(LocationTuple::build_any_location_tuple(),
+                LocationTuple::compose(&build_location_tuple_helper("L6", LocationType::Normal), &build_location_tuple_helper("L1", LocationType::Normal), CompositionType::Conjunction);
+                "_ != L6&&L1")]
+    #[test_case(LocationTuple::build_any_location_tuple(),
+                LocationTuple::compose(&LocationTuple::build_any_location_tuple(), &LocationTuple::build_any_location_tuple(), CompositionType::Conjunction);
+                "anylocation _ != _&&_")]
+    #[test_case(LocationTuple::compose(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L4", LocationType::Normal), CompositionType::Conjunction),
+                LocationTuple::merge_as_quotient(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L4", LocationType::Normal));
+                "L2&&L4 != L2\\L4")]
+    #[test_case(LocationTuple::compose(&LocationTuple::compose(&LocationTuple::build_any_location_tuple(), &LocationTuple::build_any_location_tuple(), CompositionType::Composition),&build_location_tuple_helper("L2", LocationType::Normal), CompositionType::Conjunction),
+                LocationTuple::compose(&LocationTuple::compose(&build_location_tuple_helper("L2", LocationType::Normal), &build_location_tuple_helper("L1", LocationType::Normal), CompositionType::Composition),&build_location_tuple_helper("L2", LocationType::Normal), CompositionType::Composition);
+                "_||_&&L2 == L2||L1||L2")]
+    #[test_case(LocationTuple::compose(&LocationTuple::compose(&build_location_tuple_helper("L2", LocationType::Normal), &LocationTuple::build_any_location_tuple(), CompositionType::Composition),&build_location_tuple_helper("L2", LocationType::Normal), CompositionType::Conjunction),
+                LocationTuple::compose(&LocationTuple::build_any_location_tuple(), &LocationTuple::build_any_location_tuple(), CompositionType::Conjunction);
+                "L2||_&&L2 == _&&_")]
+    #[test_case(build_location_tuple_helper("L7", LocationType::Normal),
+                build_location_tuple_helper("L5", LocationType::Normal);
+                "L7 != L5")]
+    #[test_case(LocationTuple::merge_as_quotient(&LocationTuple::build_any_location_tuple(), &LocationTuple::build_any_location_tuple()),
+                LocationTuple::compose(&build_location_tuple_helper("L6", LocationType::Normal), &build_location_tuple_helper("L25", LocationType::Normal), CompositionType::Conjunction);
+                "_//_ != L6&&L25")]
+    #[test_case(build_location_tuple_helper("_L1", LocationType::Normal),
+                build_location_tuple_helper("L1", LocationType::Normal);
+                "_L1 != L1")]
+    #[test_case(build_location_tuple_helper("__", LocationType::Normal),
+                build_location_tuple_helper("L7", LocationType::Normal);
+                "__ != L7")]
+    fn checks_cmp_locations_returns_false(loc1: LocationTuple, loc2: LocationTuple) {
+        assert!(!loc1.compare_partial_locations(&loc2));
+    }
 }
-
-// #[macro_export]
-// macro_rules! test1{
-//    ( ($($left:expr), +) (($op:"||"), +) (($right:expr), +)) == $($left1:expr)$($op:"||")$($right1:expr) ) =>{
-
-//     }
-// }
