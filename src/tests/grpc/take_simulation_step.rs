a0562bb1c9d4464f3fcfb164480c3b363095affa
Casper Ståhl <cstahl20@student.aau.dk>
2022-11-29 09:26:04 +0100

    Combine test cases

--- src/tests/grpc/take_simulation_step.rs
+++ src/tests/grpc/take_simulation_step.rs
@@ -19,31 +19,20 @@ mod test {
     use test_case::test_case;
     use tonic::{self, Request, Response, Status};
 
-    #[ignore = "Server hangs on panic"]
     #[test_case(
         create_good_request(),
         create_expected_response_to_good_request();
         "given a good request, responds with correct state"
     )]
     #[test_case(
-        create_mismatched_request_1(),
-        create_expected_response_to_mismatched_request_1();
-        "given a request with component decision mismatch, decision referencing source not in the set of states, responds with invalid argument"
+        create_composition_request(),
+        create_expected_response_to_composition_request();
+        "given a composition request, responds with correct component"
     )]
     #[test_case(
-        create_mismatched_request_2(),
-        create_expected_response_to_mismatched_request_2();
-        "given a request with component decision mismatch, decision making transition that is not possible, responds with invalid argument"
-    )]
-    #[test_case(
-        create_malformed_component_request(),
-        create_response_to_malformed_component_request();
-        "given a request with a malformed component, responds with invalid argument"
-    )]
-    #[test_case(
-        create_malformed_composition_request(),
-        create_response_to_malformed_composition_request();
-        "given a request with a malformed composition, responds with invalid argument"
+        create_conjunction_request(),
+        create_expected_response_to_conjunction_request();
+        "given a good conjunction request, responds with correct component"
     )]
     #[tokio::test]
     async fn take_simulation_step__responds_as_expected(
@@ -61,6 +50,37 @@ mod test {
             format!("{:?}", expected_response),
             format!("{:?}", actual_response)
         );
+    }
+
+    #[ignore = "Server hangs on panic"]
+    #[test_case(
+        create_mismatched_request_1();
+        "given a request with component decision mismatch, decision referencing source not in the set of states, responds with invalid argument"
+    )]
+    #[test_case(
+        create_mismatched_request_2();
+        "given a request with component decision mismatch, decision making transition that is not possible, responds with invalid argument"
+    )]
+    #[test_case(
+        create_malformed_component_request();
+        "given a request with a malformed component, responds with invalid argument"
+    )]
+    #[test_case(
+        create_malformed_composition_request();
+        "given a request with a malformed composition, responds with invalid argument"
+    )]
+    #[tokio::test]
+    async fn start_simulation__bad_data__responds_with_error(
+        request: Request<SimulationStepRequest>,
+    ) {
+        // Arrange
+        let backend = ProtobufServer::ConcreteEcdarBackend::default();
+
+        // Act
+        let actual_response = backend.take_simulation_step(request).await;
+
+        // Assert
+        assert!(actual_response.is_err());
     }
 
     // A request that Chooses the FAT EDGE:
@@ -165,32 +185,6 @@ mod test {
         Err(Status::invalid_argument(
             "Malformed composition, bad expression",
         ))
-    }
-
-    #[test_case(
-        create_composition_request(),
-        create_expected_response_to_composition_request();
-        "given a composition request, responds with correct component"
-    )]
-    #[test_case(
-        create_conjunction_request(),
-        create_expected_response_to_conjunction_request();
-        "given a good conjunction request, responds with correct component"
-    )]
-    #[tokio::test]
-    async fn take_simulation_step__get_composit_component__should_return_component(
-        request: Request<SimulationStepRequest>,
-        expected_response: Result<Response<SimulationStepResponse>, Status>,
-    ) {
-        let backend = ProtobufServer::ConcreteEcdarBackend::default();
-
-        let actual_response = backend.take_simulation_step(request).await;
-
-        // Assert
-        assert_eq!(
-            format!("{:?}", expected_response),
-            format!("{:?}", actual_response)
-        );
     }
 
     // A || B || C
